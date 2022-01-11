f05198c42771e82dc97bd431f66f3c2a64cbc490
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-01-11 09:43:01 +0700

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Sub Kosong()
+        tbKodeJenis.Clear()
+        tbNamaJenis.Clear()
+        TextBox3.Clear()
+    End Sub
+    Sub Isi()
+        tbNamaJenis.Clear()
+        tbNamaJenis.Focus()
+    End Sub
+    Sub TampilJenis()
+        da = New OleDbDataAdapter("Select * From Jenis", Conn)
+        ds = New DataSet
+        ds.Clear()
+        da.Fill(ds, "Jenis")
+        DataGridView1.DataSource = ds.Tables("Jenis")
+        DataGridView1.Refresh()
+    End Sub
+    Sub AturGrid()
+        DataGridView1.Columns(0).Width = 60
+        DataGridView1.Columns(1).Width = 200
+        DataGridView1.Columns(0).HeaderText = "Kode Jenis"
+        DataGridView1.Columns(1).HeaderText = "Nama Jenis"
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
